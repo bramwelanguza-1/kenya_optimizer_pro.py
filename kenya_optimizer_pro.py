92b38240988851a4718fb438d19111aa882fafa2
bramwelanguza-1 <bramwelanguza@gmail.com>
2026-01-30 20:24:12 +0300

    Update kenya_optimizer_pro.py

--- kenya_optimizer_pro.py
+++ kenya_optimizer_pro.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import folium
-from streamlit_folium import st_folium
+from streamlit_folium import folium_static
 import plotly.express as px
 import random
 import time
